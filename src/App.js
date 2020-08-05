f49a5ce80042cdaa182261a89f236a8e17ca284b
kawaji michihiro <michikun06@icloud.com>
2020-08-05 17:09:18 +0900

    first react-hook useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const multiplication = () => setCount(count * 2)
+  const division3 = () => setCount(previousCount =>
+    previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  )
+
   return (
-    <div>
-      This is a templete for React app.
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>＋１</button>
+        <button onClick={decrement}>ー１</button>
+      </div>
+      <div>
+        <button onClick={increment2}>＋１</button>
+        <button onClick={decrement2}>ー１</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={multiplication}>×２</button>
+        <button onClick={division3}>÷３</button>
+      </div>
+    </React.Fragment>
   );
 }
 
