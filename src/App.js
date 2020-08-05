a7729595d3202383ab7a00e52122b6a5874b9ee7
kawaji michihiro <michikun06@icloud.com>
2020-08-06 01:21:39 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ const App = props => {
   return (
     <>
       <p>現在の{state.name}は,{state.price}円です。</p>
-      <button onClick={() => setState({ ...state, price: state.price + 1 })}>+1</button>
-      <button onClick={() => setState({ ...state, price: state.price - 1 })}>-1</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
       <button onClick={() => setState(props)}>reset</button>
       <input value={state.name} onChange={e => setState({ ...state, name: e.target.value })} />
     </>
