9522b3b3f7d31dd7b2174e736cabc88822475db5
kawaji michihiro <michikun06@icloud.com>
2020-08-05 22:42:33 +0900

    implementobjec state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { useState } from 'react';
 
 const App = props => {
   // nameとpriceのstateを作成
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+  // const [name, setName] = useState(props.name)
+  // const [price, setPrice] = useState(props.price)
+  // 外部で設定したものを次の一行でまとめて引っ張ってこれる
+  const [state, setState] = useState(props)
+  const { name, price } = state
 
   return (
     <>
-      <p>現在の{name}は,{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <p>現在の{state.name}は,{state.price}円です。</p>
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: state.price - 1 })}>-1</button>
+      <button onClick={() => setState(props)}>reset</button>
+      <input value={state.name} onChange={e => setState({ ...state, name: e.target.value })} />
     </>
   );
 }
