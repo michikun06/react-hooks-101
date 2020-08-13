38c45b345e37372eed95bdb45d3cb9d86f86e77a
kawaji michihiro <michikun06@icloud.com>
2020-08-13 13:57:21 +0900

    solve prop drilling issue

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 // DELETE_EVENTがタイポの場合エラーが出ずにバグが発生し、見つけるのが困難
 // 下記のように外部から入れることでタイポ、バグを即座に発見することができる
 import { DELETE_EVENT } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const Event = ({ event, dispatch }) => {
+const Event = ({ event }) => {
+    const { dispatch } = useContext(AppContext)
     const id = event.id
     // 個別のイベントを削除
     const handleClickDeleteButton = () => {
